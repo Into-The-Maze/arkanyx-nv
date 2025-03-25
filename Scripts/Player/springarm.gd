3e6b70b1988e1e7004bc85b363c597ab7efed705
Long <dylant7192@gmail.com>
2025-03-25 23:42:58 +0000

    Start of smooth cam interpolation

--- Scripts/Player/springarm.gd
+++ Scripts/Player/springarm.gd
@@ -1,8 +1,10 @@
-extends SpringArm3D
+extends Node3D
 
 @export var mouse_sensitivity: float = 0.005
 @export_range(-90., 0., 0.1, "radians_as_degrees") var min_v_axis: float = -PI/2
 @export_range(0., 90., 0.1, "radians_as_degrees") var max_v_axis: float = PI/4
+
+@onready var spring_target: Node3D = $SpringTarget
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
