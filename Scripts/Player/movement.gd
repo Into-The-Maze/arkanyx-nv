5de3e480d0a68ed57eca640ced07b347fb83df4a
Jonty <89488176+Jomlom@users.noreply.github.com>
2025-03-27 18:23:27 +0000

    reduce air strafing

--- Scripts/Player/movement.gd
+++ Scripts/Player/movement.gd
@@ -2,22 +2,24 @@ extends CharacterBody3D
 
 @export var max_speed: float = 6.0
 @export var acceleration: float = 30.0
-@export var gravity: float = 15
+@export var gravity: float = 20
 @export var max_jump_height: float = 3.0
-@export var jump_charge_max_time: float = 1
+@export var jump_charge_max_time: float = 0.5
 @export var jump_deadzone: float = 0.15
+@export var air_strafe_reduction_multiplier: float = 3
 
 @onready var camera: Camera3D = $Pivot/Camera
 @onready var jump_charge_bar: ProgressBar = $CanvasLayer/ProgressBar
 
 var jump_charge_timer: float = 0.0
+var is_jumping: bool = false
 
 func _ready() -> void:
 	pass 
 
 func _process(delta: float) -> void:
 	if jump_charge_bar:
-		jump_charge_bar.value = max(0, (jump_charge_timer-jump_deadzone / jump_charge_max_time-jump_deadzone) * 100)
+		jump_charge_bar.value = max(0, ((jump_charge_timer - jump_deadzone) / (jump_charge_max_time - jump_deadzone) * 100))
 
 func _physics_process(delta: float) -> void:
 	
@@ -38,30 +40,36 @@ func _physics_process(delta: float) -> void:
 	
 	var horizontal_velocity = Vector3(current_velocity.x, 0, current_velocity.z)
 	var target_horizontal_velocity = direction * max_speed
-	horizontal_velocity = horizontal_velocity.move_toward(target_horizontal_velocity, acceleration * delta)
+	var adjusted_acceleration = acceleration
+	if not is_on_floor():
+		adjusted_acceleration /= air_strafe_reduction_multiplier
+	horizontal_velocity = horizontal_velocity.move_toward(target_horizontal_velocity, adjusted_acceleration * delta)
 	current_velocity.x = horizontal_velocity.x
 	current_velocity.z = horizontal_velocity.z
 	
 	# GRAVITY
 	if not is_on_floor():
 		current_velocity.y -= gravity * delta
+		if is_jumping && current_velocity.y < 0:
+			current_velocity.y -= gravity * delta
 	else:
 		current_velocity.y = 0
 	
 	# JUMPING
 	if is_on_floor():
+		is_jumping = false
 		if Input.is_action_pressed("jump"):
-			jump_charge_timer = min(jump_charge_timer + delta, jump_charge_max_time)
+			jump_charge_timer = min(jump_charge_timer + delta, jump_charge_max_time + jump_deadzone)
 		elif Input.is_action_just_released("jump"):
-			var multiplier = 0.4
+			var multiplier = 0.5
 			var adjusted_jump_timer = jump_charge_timer - jump_deadzone
 			if adjusted_jump_timer > 0:
-				multiplier += adjusted_jump_timer
+				multiplier += pow((adjusted_jump_timer / jump_charge_max_time - jump_deadzone), 2) * 0.5
 			var jump_speed = sqrt(2 * gravity * (max_jump_height * multiplier))
 			current_velocity.y = jump_speed
+			is_jumping = true
 			jump_charge_timer = 0.0
 		else:
-			# Reset charge if no jump input is provided.
 			jump_charge_timer = 0.0
 	
 	# APPLYING MOVEMENT
