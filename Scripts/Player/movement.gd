1c5e7d110176eccb81c0d06f13bb9c9940ac4017
Jonty <89488176+Jomlom@users.noreply.github.com>
2025-03-27 22:53:56 +0000

    wall holding + dodging
    also some simple scripts for the UI progress bars, just for more intuitive visuals

--- Scripts/Player/movement.gd
+++ Scripts/Player/movement.gd
@@ -20,18 +20,25 @@ extends CharacterBody3D
 @export var dodge_penalty_slowdown_duration: float = 0.5
 @export var air_dodge_reduction_multiplier: float = 0.7
 
+# WALL HOLDING PARAMETERS
+@export var max_wall_hold_time: float = 1.0
+
 # REFERENCES
 @onready var camera: Camera3D = $Pivot/Camera
 @onready var jump_charge_bar: ProgressBar = $CanvasLayer/JumpChargeBar
 @onready var dodge_cooldown_bar: ProgressBar = $CanvasLayer/DodgeCooldownBar
+@onready var wall_hold_bar: ProgressBar = $CanvasLayer/WallHoldBar
 
 var jump_charge_timer: float = 0.0
 var dodge_cooldown_timer: float = 0.0
 var dodge_penalty_timer: float = 0.0
+var wall_hold_timer: float = 0.0
 var is_jumping: bool = false
 var is_sprinting: bool = false
 var is_dodging: bool = false
-var is_wall_jumping: bool = false
+var is_air_dodging: bool = false
+var is_wall_holding: bool = false
+var is_wall_dodging: bool = false
 
 func _ready() -> void:
 	pass
@@ -41,15 +48,31 @@ func _process(delta: float) -> void:
 		jump_charge_bar.value = max(0, ((jump_charge_timer - jump_deadzone) / (jump_charge_max_time - jump_deadzone) * jump_charge_bar.max_value))
 	if dodge_cooldown_bar:
 		dodge_cooldown_bar.value = max(0, (dodge_cooldown_timer / dodge_cooldown) * dodge_cooldown_bar.max_value)
+	if wall_hold_bar:
+		if is_wall_holding:
+			wall_hold_bar.value = max(0, (1 - (wall_hold_timer / max_wall_hold_time)) * wall_hold_bar.max_value)
+		else:
+			wall_hold_bar.value = 0
 
 func _physics_process(delta: float) -> void:
 	
 	# Adjust timers and flags
 	if is_dodging:
 		is_dodging = !is_on_floor()
+	if is_air_dodging:
+		is_air_dodging = !is_on_floor()
 	if not is_dodging:
 		dodge_cooldown_timer = max(dodge_cooldown_timer - delta, 0)
 		dodge_penalty_timer = max(dodge_penalty_timer - delta, 0)
+	if is_wall_holding:
+		wall_hold_timer = min(wall_hold_timer + delta, max_wall_hold_time)
+	if wall_hold_timer >= max_wall_hold_time:
+		is_wall_holding = false
+	if is_on_floor() && not is_wall_holding:
+		wall_hold_timer = 0
+	if is_wall_dodging:
+		is_wall_dodging = !is_on_floor()
+		is_wall_holding = false
 	
 	# MOVING
 	var adjusted_max_speed = max_speed
@@ -83,15 +106,15 @@ func _physics_process(delta: float) -> void:
 	current_velocity.z = horizontal_velocity.z
 	
 	# GRAVITY
-	if not is_on_floor():
+	if not is_on_floor() && not is_wall_holding:
 		current_velocity.y -= gravity * delta
-		if is_jumping && current_velocity.y < 0:
+		if current_velocity.y < 0:
 			current_velocity.y -= gravity * delta
 	else:
 		current_velocity.y = 0
 	
 	# JUMPING
-	if is_on_floor():
+	if is_on_floor() && not is_wall_holding:
 		is_jumping = false
 		if Input.is_action_pressed("jump"):
 			jump_charge_timer = min(jump_charge_timer + delta, jump_charge_max_time + jump_deadzone)
@@ -119,8 +142,15 @@ func _physics_process(delta: float) -> void:
 		else:
 			jump_charge_timer = 0.0
 	
+	# WALL HOLDING
+	if is_on_wall() && not is_on_floor() && Input.is_action_pressed("jump") && not is_wall_dodging && (wall_hold_timer < max_wall_hold_time):
+		is_wall_holding = true
+		current_velocity = Vector3.ZERO
+	else:
+		is_wall_holding = false
+	
 	# DODGING 
-	if Input.is_action_just_pressed("dodge") && dodge_cooldown_timer <= 0:
+	if Input.is_action_just_pressed("dodge") && dodge_cooldown_timer <= 0 && not is_wall_dodging:
 		if direction == Vector3.ZERO:
 			var current_horizontal = Vector3(current_velocity.x, 0, current_velocity.z)
 			if current_horizontal.length() > 0:
@@ -129,8 +159,14 @@ func _physics_process(delta: float) -> void:
 				direction = (-camera.global_transform.basis.z).normalized()
 		horizontal_velocity = direction * max_speed * dodge_speed_multiplier
 		# AIR DODGING
-		if not is_on_floor():
+		if not is_on_floor() && not is_wall_holding:
 			horizontal_velocity *= air_dodge_reduction_multiplier
+			is_air_dodging = true
+		# WALL DODGING
+		if is_wall_holding:
+			is_wall_dodging = true
+			var jump_speed = sqrt(2 * gravity * (max_jump_height))
+			current_velocity.y = jump_speed
 		current_velocity.x = horizontal_velocity.x
 		current_velocity.z = horizontal_velocity.z
 		is_dodging = true
