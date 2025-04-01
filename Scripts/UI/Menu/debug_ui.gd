fb2e7721fa1fffcb57310b38fb6dde5823af9c1d
Jonty <89488176+Jomlom@users.noreply.github.com>
2025-04-01 20:33:16 +0100

    flags and timers on improved debugging UI

--- Scripts/UI/Menu/debug_ui.gd
+++ Scripts/UI/Menu/debug_ui.gd
@@ -2,9 +2,11 @@ extends CanvasLayer
 
 @export var target_stats: StatBlock
 @onready var player: CharacterBody3D = $".."
-@onready var container: VBoxContainer = $Container
+@onready var container_l: VBoxContainer = $LeftContainer
+@onready var container_r: VBoxContainer = $RightContainer
 
 var stat_labels: Dictionary = {}
+var local_debug_labels: Dictionary = {}
 
 func _ready() -> void:
 	pass
@@ -39,5 +41,40 @@ func _process(delta: float) -> void:
 			new_label.name = stat_name
 			new_label.text = "%s = ( BASE %.2f + FLAT ADD %.2f ) * MULTIPLIER %.2f" % [stat_name, base_val, flat_total, effective_multiplier]
 			new_label.add_theme_color_override("font_color", Color.GREEN)
-			container.add_child(new_label)
+			container_l.add_child(new_label)
 			stat_labels[stat_name] = new_label
+		
+		var debug_info = {
+		"jump_charge_timer": player.jump_charge_timer,
+		"dodge_cooldown_timer": player.dodge_cooldown_timer,
+		"dodge_penalty_timer": player.dodge_penalty_timer,
+		"wall_hold_timer": player.wall_hold_timer,
+		"is_jumping": player.is_jumping,
+		"is_sprinting": player.is_sprinting,
+		"is_dodging": player.is_dodging,
+		"is_air_dodging": player.is_air_dodging,
+		"is_wall_holding": player.is_wall_holding,
+		"is_wall_dodging": player.is_wall_dodging,
+		"velocity": player.velocity 
+		}
+		
+		for key in debug_info.keys():
+			var value = debug_info[key]
+			if typeof(value) == TYPE_VECTOR3:
+				value = "x%.2f, y%.2f, z%.2f" % [value.x, value.y, value.z]
+			elif typeof(value) == TYPE_BOOL:
+				value = str(value)
+			elif typeof(value) in [TYPE_FLOAT, TYPE_INT]:
+				value = "%.2f" % value
+			
+			var debug_string = "%s: %s" % [key, value]
+			if local_debug_labels.has(key):
+				local_debug_labels[key].text = debug_string
+			else:
+				var new_label = Label.new()
+				new_label.name = key
+				new_label.text = debug_string
+				new_label.add_theme_color_override("font_color", Color.RED)
+				new_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+				container_r.add_child(new_label)
+				local_debug_labels[key] = new_label
