64d50f060454133d8ae42387eaed3d38968aaab2
Long <dylant7192@gmail.com>
2025-04-01 18:09:27 +0100

    Moved item dragging functionality to simplify code

--- Scripts/Inventory/inventory_item_mouse_ui.gd
+++ Scripts/Inventory/inventory_item_mouse_ui.gd
@@ -1,18 +1,16 @@
-extends Control
-
-@onready var selectedItemImage: Sprite2D = $SelectedItemImage
+extends Sprite2D
 
 func _ready():
 	SignalBus.connect("INVENTORY_ITEM_SELECTED", select_item.bind())
 	SignalBus.connect("INVENTORY_ITEM_PLACED", insert_item.bind())
 
 func _process(_delta):
-	if selectedItemImage.texture != null:
-		selectedItemImage.position = get_global_mouse_position()
+	if texture != null:
+		position = get_global_mouse_position()
 
 func select_item(_id, item_display):
-	selectedItemImage.texture = item_display
+	texture = item_display
 
 func insert_item(_id):
-	selectedItemImage.texture = null
+	texture = null
 	
