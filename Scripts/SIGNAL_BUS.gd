cf115951738f2e963214cbd63339c089ace21276
Long <dylant7192@gmail.com>
2025-04-06 17:26:17 +0100

    Neatened up SIGNALBUS

--- Scripts/SIGNAL_BUS.gd
+++ Scripts/SIGNAL_BUS.gd
@@ -1,12 +1,31 @@
 extends Node
 
+# inventory signals
+@warning_ignore("unused_signal")
 signal INVENTORY_OPENED
+
+@warning_ignore("unused_signal")
 signal INVENTORY_CLOSED
+
+@warning_ignore("unused_signal")
 signal INVENTORY_ITEM_SELECTED
+
+@warning_ignore("unused_signal")
 signal INVENTORY_ITEM_PLACED
+
+@warning_ignore("unused_signal")
 signal INVENTORY_ITEM_SWAPPED
+
+@warning_ignore("unused_signal")
 signal INVENTORY_SELECTED
+
+@warning_ignore("unused_signal")
 signal INVENTORY_UPDATE
 
+# menu signals
+@warning_ignore("unused_signal")
 signal MENU_OPENED
+
+@warning_ignore("unused_signal")
 signal MENU_CLOSED
+
