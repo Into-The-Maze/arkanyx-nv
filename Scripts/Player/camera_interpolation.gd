d0d2512b6b7e0224bb3c9fc7aaf3f34d1c1e9381
Long <dylant7192@gmail.com>
2025-04-09 18:20:23 +0100

    Fixed all camera bugs

--- Scripts/Player/camera_interpolation.gd
+++ Scripts/Player/camera_interpolation.gd
@@ -3,5 +3,11 @@ extends Camera3D
 @export var spring_target: Node3D
 @export var camera_velocity: float = 1.
 
+@onready var pivot = $".."
+
 func _process(delta: float) -> void:
+	if pivot.position.distance_to(position) >= pivot.position.distance_to(spring_target.position):
+		position = spring_target.position
+		return
+
 	position = lerp(position, spring_target.position, delta * camera_velocity)
