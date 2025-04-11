1701b9db77d342620c3e10ef9816d224d5a36102
Jonty <89488176+Jomlom@users.noreply.github.com>
2025-04-11 18:18:46 +0100

    descriptions + close button to popup

--- Scripts/Inventory/item_details_popup.gd
+++ Scripts/Inventory/item_details_popup.gd
@@ -5,13 +5,17 @@ class_name ItemDetailsPopup
 @onready var icon = $VBox/Icon_and_Name/Icon
 @onready var name_label = $VBox/Icon_and_Name/Name
 @onready var description_label = $VBox/Description
+@onready var close_button = $Close
+
+func _ready():
+	close_button.connect("pressed", _on_close_button_pressed)
 
 func show_item(item: Inventory_Item, position: Vector2):
 	icon.texture = item.icon
 	scale_texture_to_64px(icon.texture)
 	
 	name_label.text = item.name
-	description_label.text = "This is a placeholder item description for testing."
+	description_label.text = item.description
 	description_label.minimum_size_changed
 	
 	mainVBox.queue_sort
@@ -21,6 +25,7 @@ func show_item(item: Inventory_Item, position: Vector2):
 	position += Vector2(128, -64)
 	global_position = position
 	visible = true
+	set_process_input(true)
 
 func scale_texture_to_64px(texture: Texture):
 	if texture:
@@ -36,3 +41,7 @@ func update_panel_size():
 	var size = mainVBox.get_minimum_size()
 	custom_minimum_size = size
 	position = global_position - custom_minimum_size / 2
+
+func _on_close_button_pressed():
+	visible = false
+	set_process_input(false)
