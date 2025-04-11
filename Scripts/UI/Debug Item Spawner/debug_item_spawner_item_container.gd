23ba26381ae56ed400d492879a70ba37b4562426
Long <dylant7192@gmail.com>
2025-04-11 17:45:22 +0100

    Fixed issue where the Sprite2D node was being cast to a Texture2D without any error

--- Scripts/UI/Debug Item Spawner/debug_item_spawner_item_container.gd
+++ Scripts/UI/Debug Item Spawner/debug_item_spawner_item_container.gd
@@ -1,11 +1,12 @@
 extends CenterContainer
 
-@onready var item_texture = $ItemTexture
+@onready var item_texture: Sprite2D = $ItemTexture
 
 var item: Inventory_Item
 
 func _ready():
-	item_texture = item.icon
+	item_texture.texture = item.icon
 
 func _on_spawn_button_down() -> void:
+	print_debug("spawning: ", item.name)
 	SIGNAL_BUS.emit_signal("DEBUG_INSERT_NEW_ITEM", item)
