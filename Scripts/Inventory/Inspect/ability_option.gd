44ad0788f4a3a3cd2f15dce1e37089ebaf1e3f38
Jonty <89488176+Jomlom@users.noreply.github.com>
2025-04-12 09:50:13 +0100

    actual ability select function

--- Scripts/Inventory/Inspect/ability_option.gd
+++ Scripts/Inventory/Inspect/ability_option.gd
@@ -1,6 +1,16 @@
 extends Panel
 
 @export var icon: Sprite2D
+@export var ability: Ability
 
-func set_ability(ability: Ability):
-	icon.texture = ability.icon
+func set_ability(newAbility: Ability):
+	if !newAbility:	
+		ability = null
+		icon.texture = preload("res://Assets/Temporary/inventory_drop_item.png")
+	else:
+		ability = newAbility
+		icon.texture = ability.icon
+
+func _on_button_pressed() -> void:
+	if get_parent().get_parent().has_method("selected_ability"):
+		get_parent().get_parent().selected_ability(ability)
--- Scripts/Inventory/Inspect/ability_slot_selectable.gd
+++ Scripts/Inventory/Inspect/ability_slot_selectable.gd
@@ -1,6 +1,5 @@
 extends Panel
 
-@onready var parent: HBoxContainer = $".."
 @export var ability_icon: Sprite2D
 @export var slot_icon: Sprite2D
 @export var abilities_grid: GridContainer
@@ -14,19 +13,34 @@ func set_slot(newSlot: AbilitySlot):
 	slot_icon.texture = slot.slot_icon
 	if slot.assigned_ability:
 		ability_icon.texture = slot.assigned_ability.icon
+	else:
+		ability_icon.texture = null
 
 func _on_button_pressed() -> void:
 	var option: PackedScene = preload("res://Scenes/Inventory/Inspect/ability_option.tscn")
-	for ability_slot_selectable in parent.get_children():
-		if ability_slot_selectable.has_method("clear_options"):
-			ability_slot_selectable.clear_options()
+	for child in get_parent().get_children():
+		if child.has_method("clear_options"):
+			child.clear_options()
+	
+	var empty_option = option.instantiate()
+	empty_option.set_ability(null)
+	abilities_grid.add_child(empty_option)
 	
 	for ability in learned_abilities.abilities:
 		var new_option = option.instantiate()
 		new_option.set_ability(ability)
 		abilities_grid.add_child(new_option)
+	
 	abilities_grid.visible = true
 
 func clear_options():
 	for child in abilities_grid.get_children():
 		child.queue_free()
+
+func selected_ability(selected: Ability):
+	if selected:
+		slot.assigned_ability = selected
+	else:
+		slot.assigned_ability = null
+	clear_options()
+	set_slot(slot)
