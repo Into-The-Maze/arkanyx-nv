23b2dcc1f43b94f9c7c0a2bacc94fe9cd033c16d
Jonty <89488176+Jomlom@users.noreply.github.com>
2025-04-13 00:05:41 +0100

    super visual improvements
    new sprites + descriptions
    tooltips added

--- Scripts/Inventory/Inspect/ability_option.gd
+++ Scripts/Inventory/Inspect/ability_option.gd
@@ -2,14 +2,17 @@ extends Panel
 
 @export var icon: Sprite2D
 @export var ability: Ability
+@export var button: Button
 
 func set_ability(newAbility: Ability):
 	if !newAbility:	
 		ability = null
 		icon.texture = preload("res://Abilities/deselect.png")
+		button.tooltip_text = "Remove the selected ability."
 	else:
 		ability = newAbility
 		icon.texture = ability.icon
+		button.tooltip_text = ability.description
 
 func _on_button_pressed() -> void:
 	if get_parent().get_parent().has_method("selected_ability"):
